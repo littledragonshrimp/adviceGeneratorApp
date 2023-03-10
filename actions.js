bdddcbc5a1ad7b4366d83ccefb0f78c08738c3a9
Jordan <JDLChang@gmail.com>
2023-03-10 06:55:58 -0600

    adds desktop mode, adds fixed js connection to api, adds footer, removes border

--- actions.js
+++ actions.js
@@ -1,20 +1,38 @@
-const baseURL =  
-{ ref   : 'https://api.adviceslip.com/advice'
-, count : 0
-};
+//  const baseURL =  
+//  { ref   : 'https://api.adviceslip.com/advice'
+//  , count : 0
+//  };
 
 const rollDice        = document.getElementById('reload');
-const adviceNumber    = document.getElementById('id');
-const adviceParagraph = document.getElementById('advice');
+const adviceNumber    = document.getElementById('adviceID');
+const adviceParagraph = document.getElementById('adviceText');
 
-rollDice.onclick = () =>
-    {
-        console.log("clicked");
-        fetch( `${baseURL.ref}?c=${++baseURL.count}`)
-        .then( r => r.json() )
-        .then( data =>
-            {
-                adviceNumber.textContent = data.slip.id
-                adviceParagraph.textContent = data.slip.adviceText
-            })
-    }
+
+function loadQuote() {
+    fetch("https://api.adviceslip.com/advice")
+    .then((response) => {
+        return response.json();
+    })
+
+    .then((response) => {
+        adviceNumber.innerHTML = `Advice #${response.slip.id}`;
+        adviceParagraph.innerHTML = `&#8220${response.slip.advice}&#8221`;
+      });
+}
+
+rollDice.addEventListener("click", loadQuote);
+console.log("clicked");
+
+
+// rollDice.onclick = () =>
+//     {
+//         console.log("clicked");
+//         fetch( `${baseURL.ref}?c=${++baseURL.count}`)
+//         .then( r => r.json() )
+//         .then( data =>
+//             {
+//                 adviceNumber.textContent = data.slip.id
+//                 adviceParagraph.textContent = data.slip.advice
+//             })
+//     }
+
